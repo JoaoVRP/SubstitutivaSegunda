518ff45fb94db5ec8586979205ac1d31e5cb2a2c
Diogo Deconto <dideconto@gmail.com>
2021-09-13 22:07:20 -0300

    Integração com a API e listagem de produtos

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Produto } from "./models/produto";
+import { ProdutoService } from "./services/produto.service";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: [],
 })
-export class AppComponent {
-    title = "FRONT ALTERADO";
+export class AppComponent implements OnInit {
+    produtos: Produto[] = [];
+
+    constructor(private service: ProdutoService) {}
+
+    ngOnInit(): void {
+        this.service.list().subscribe((produtos) => {
+            this.produtos = produtos;
+            console.log(produtos);
+        });
+    }
 }
