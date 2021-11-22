e87805b703b1a21f1d4f16f590ac239826b34456
Diogo Deconto <dideconto@gmail.com>
2021-11-22 19:42:28 -0300

    Implementação da autenticação e autorização por JWT na API - Parte I

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,7 +41,7 @@ namespace API
 
             //Configuração do serviço de banco de dados
             services.AddDbContext<DataContext>(
-                options => options.UseSqlServer(Configuration.GetConnectionString("Azure"))
+                options => options.UseInMemoryDatabase(Configuration.GetConnectionString("Azure"))
             );
             services.AddControllers();
             services.AddSwaggerGen(c =>
